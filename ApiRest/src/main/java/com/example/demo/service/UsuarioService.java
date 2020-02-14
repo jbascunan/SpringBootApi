f876eecd5fd69932aefa07e1e8c61b1444da3555
Jose Bascuñan Pinilla <jbascunan@ministeriodeeconomia.onmicrosoft.com>
2020-02-13 22:42:29 -0300

    modificacion

--- ApiRest/src/main/java/com/example/demo/service/UsuarioService.java
+++ ApiRest/src/main/java/com/example/demo/service/UsuarioService.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.exception.ExceptionNotFound;
-import com.example.demo.hibernate.util.HibernateUtil;
 import com.example.demo.model.UsuarioEntity;
 import com.example.demo.repository.UsuarioRepository;
 import com.example.demo.security.JwtTokenUtil;
